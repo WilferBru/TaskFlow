0eb6d71de661bcc1b7a3ebfec0a736cef5eb89a4
WilferBru <wilferaiwe@gmail.com>
2025-10-26 16:01:50 -0500

    Armando la pagina pase del rpoeycto

--- frontend/vue-SPA/src/components/layout/SidebarAdmin.vue
+++ frontend/vue-SPA/src/components/layout/SidebarAdmin.vue
@@ -1,10 +1,10 @@
 <template>        
-     <aside class="w-20 bg-white dark:bg-black shadow-md p-3">
+     <aside class="w-20 dark:bg-black shadow-md p-3">
       <nav class="space-y-2 flex flex-col h-full">
         <RouterLink
           :to="{ name: 'home' }"
           class="flex flex-col items-center px-3 py-3 rounded text-gray-300 hover:bg-gray-600"
-          :class="{ 'text-green-400 font-semibold': $route.name === 'home' }"
+          :class="{ 'text-sky-500 font-semibold bg-gray-700': $route.name === 'home' }"
         >
             <HomeIcon />
             <p class="text-xs mt-2">Home</p>
@@ -13,7 +13,7 @@
         <RouterLink
           :to="{ name: 'task' }"
           class="flex flex-col items-center px-3 py-3 rounded text-gray-300 hover:bg-gray-600"
-          :class="{ 'text-green-400 font-semibold': $route.name === 'task' }"
+          :class="{ 'text-sky-500 font-semibold bg-gray-700': $route.name === 'task' }"
         >
             <TaskIcon />
             <p class="text-xs mt-2">Tareas</p>
@@ -22,7 +22,7 @@
         <RouterLink
           :to="{ name: '' }"
           class="flex flex-col items-center px-3 py-3 rounded text-gray-300 hover:bg-gray-600"
-          :class="{ 'text-green-400 font-semibold': $route.name === '' }"
+          :class="{ 'text-sky-500 font-semibold bg-gray-700': $route.name === '' }"
         >
             <UsersIcon />
             <p class="text-xs mt-2">Usuarios</p>
@@ -31,7 +31,7 @@
         <RouterLink
           :to="{ name: 'profile' }"
           class="mt-auto flex flex-col items-center rounded text-gray-300 hover:bg-gray-600"
-          :class="{ 'text-green-400 font-semibold': $route.name === 'profile' }"
+          :class="{ 'text-sky-500 font-semibold bg-gray-700': $route.name === 'profile' }"
         >
             <UserIcon class="w-10 h-10"/>
             <p class="text-xs mt-1">Mi cuenta</p>
--- frontend/vue-SPA/src/components/layout/SidebarAll.vue
+++ frontend/vue-SPA/src/components/layout/SidebarAll.vue
@@ -1,17 +1,49 @@
 <template>
-    <aside class="w-70 bg-white  shadow-md p-2">
-      <div class="flex flex-col h-full bg-gray-200 rounded-2xl">
-        <h2 class="text-2xl text-gray-300 font-bold mb-6 py-4 px-3">TaskFlow</h2>
-        <nav class="space-y-2">
-          <RouterLink
-            :to="{ name: 'home' }"
-            class="block px-4 py-2 rounded text-gray-300 hover:bg-gray-600"
-            :class="{ 'bg-gray-700 font-semibold': $route.name === 'home' }"
-          >
-            Inicio
-          </RouterLink>
-        </nav>
+  <aside class="w-75 bg-white shadow-md p-2">
+    <div class="flex flex-col h-full bg-gray-200 rounded-2xl">
+      
+      <!-- Logo + botón -->
+      <div class="flex items-center justify-between py-3 px-4 mb-4">
+        <RouterLink :to="{ name: 'home' }">
+          <h2 class="text-2xl text-gray-900 font-bold">TaskFlow</h2>
+        </RouterLink>
+
+        <RouterLink
+          :to="{ name: 'home' }"
+          class="dark:bg-black px-4 py-2 rounded-3xl text-gray-50 hover:bg-gray-600 hover:text-shadow-gray-600"
+        >
+          Crear Tarea
+        </RouterLink>
       </div>
       
-    </aside>
+      <div class="flex justify-center mb-4">   
+        <div class="relative w-60">
+          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg 
+              class="w-5 h-5 text-gray-700 dark:text-gray-700" 
+              fill="currentColor" viewBox="0 0 20 20" 
+              xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 
+              01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path>
+            </svg>
+          </div>
+
+          <input 
+            type="text" 
+            id="simple-search" 
+            class="text-sm rounded-4xl w-full pl-10 p-2.5 bg-gray-300 placeholder-gray-700 text-gray-800" 
+            placeholder="Buscar Tarea..." 
+            required
+          >
+        </div>
+      </div>
+      <SelectSidebar />
+
+    </div>
+  </aside>
 </template>
+
+
+<script setup lang="ts">
+import SelectSidebar from './SelectSidebar.vue';
+</script>
