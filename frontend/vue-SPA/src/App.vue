cb200f1bd7c7de9a7ab736525078f039e95d88e4
WilferBru <wilferaiwe@gmail.com>
2025-10-29 19:02:30 -0500

    Implementacion de spiners

--- frontend/vue-SPA/src/App.vue
+++ frontend/vue-SPA/src/App.vue
@@ -1,7 +1,13 @@
 <template>
-    <RouterView />
+  <div>
+    <SpinerGlobal v-if="authStore.loading" />
+    <RouterView v-else />
+  </div>
 </template>
 
 <script setup lang="ts">
-    //
+import { useAuthStore } from '@/stores/authStore';
+import SpinerGlobal from '@/components/common/SpinerGlobal.vue';
+
+const authStore = useAuthStore();
 </script>
