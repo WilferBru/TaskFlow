2d485fdedda10b93d2b859758a338d7e34a39933
WilferBru <wilferaiwe@gmail.com>
2025-11-02 16:43:50 -0500

    traer el get user directo del abkedn apra que no manipulen el store en el navegador

--- frontend/vue-SPA/src/App.vue
+++ frontend/vue-SPA/src/App.vue
@@ -6,8 +6,15 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { useAuthStore } from '@/stores/authStore';
 import SpinerGlobal from '@/components/common/SpinerGlobal.vue';
 
 const authStore = useAuthStore();
+
+onMounted(async () => {
+  if (authStore.token) {
+    await authStore.fetchUser(); // traer el usuario real de laravel
+  }
+});
 </script>
